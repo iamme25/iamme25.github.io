867eb45fcd4eb82aff8beab46513ffb464f40b22
iamme25 <78478730+iamme25@users.noreply.github.com>
2022-04-04 16:00:06 -0700

    reupdate

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include('admin/config/dbcon.php');
-
-function base_url($slug){
-    echo 'http://localhost/blog4u/php-blog/'.$slug;
-}
-?>
---
+++ js/script.js
@@ -0,0 +1,46 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+let themeBtn = document.querySelector('#theme-btn');
+
+if(localStorage.getItem('theme') == null){
+    localStorage.setItem('theme', 'light');
+}
+
+let localData = localStorage.getItem('theme');
+
+if(localData == 'light'){
+    document.body.classList.remove('active');
+
+}
+else if(localData == 'dark'){
+    themeBtn.classList.toggle('fa-sun');
+    document.body.classList.add('active');
+
+}
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+        localStorage.setItem('theme', 'dark');
+    }else{
+        document.body.classList.remove('active');
+        localStorage.setItem('theme', 'light');
+    }
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    let maxHeight = window.document.body.scrollHeight - window.innerHeight;
+    let percentage = ((window.scrollY) / maxHeight) * 100;
+    document.querySelector('.header .scroll-indicator').style.width = percentage + '%';
+};
